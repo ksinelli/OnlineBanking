a568e9926532e06640aa830e8861a565cf779060
ksinelli <78870654+ksinelli@users.noreply.github.com>
2021-02-13 15:27:34 -0500

    Encapsulated functions for reduced redundancy and improved general code layout

--- OnlineBanking/src/login.java
+++ OnlineBanking/src/login.java
@@ -1,16 +1,16 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class login {
 	
-	private final static Scanner scan = new Scanner(System.in);
+	static Connection conn;
 
 	public static void main(String[] args) {
 		System.out.println("Hello, and welcome to KAS Financial!\n");
+		
+		conn = utility.getDbConnection();
 		
 		startup();
 
@@ -22,167 +22,151 @@ public class login {
 		System.out.println("Type \"Reset password\" to change your password.\n");
 		System.out.println("Pro tip: capitalization doesn't matter when typing your choice :)");
 		
-		String choice = scan.nextLine().toLowerCase();
+		String choice = utility.scan.nextLine().toLowerCase();
 		
 		if (choice.equals("sign in")) {
 			signIn();
 		}
 		
 		else if (choice.equals("create account")) {
-			createAccount();
+			createAccount(conn);
 		}
 		
 		else if (choice.equals("reset password")) {
-			resetPassword();
+			resetPassword(conn);
 		}
 		
 		else {
 			System.out.println("I'm sorry, I didn't understand that.  Please try again.\n");
 			startup();
-		}
-		
+		}	
 	}
 	
-	
 	public static void signIn() {
+		String username = getUsername(conn);
+		getPassword(conn, username);
+		dashboard.dashboardMenu();
+	}
+	
+	public static void resetPassword(Connection conn) {
+		String username = getUsername(conn);
+		getPassword(conn, username);
+		updatePassword(conn, username);		
+	}
+	
+	//retrieve entered username from database and if it does not exist, redirect to home page.
+	public static String getUsername(Connection conn) {
+			
 		System.out.println("Please enter your username.  Type \"Home\" to return to the main screen.");
-		
-		String username = scan.nextLine().toLowerCase();
-		
+			
+		String username = utility.scan.nextLine().toLowerCase();
+			
 		if (username.equals("home")) {
-			startup();
+				login.startup();
 		}
-		
-		//Connect to database.  Compare entered username and password to database values and redirect appropriately.
+			
 		try {
-			
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://ziggy.db.elephantsql.com/wxjpiraq","wxjpiraq","D6DpgE-lOUMD-QRrIBLiSUBhmxs5FW-_");
-			
+				
 			Statement stmt = conn.createStatement();
-			
+				
 			ResultSet resultSet = stmt.executeQuery("select username from logins where username = '"+username+"'");
-			
+				
 			if (resultSet.next() == false) {
-				System.out.println("That username does not exist in our system.  Please check your spelling and try again or create a new account.\n");
-				signIn();
+				System.out.println("That username does not exist in our system.\n");
+				login.signIn();
 			}
 			
-			System.out.println("Please enter your password.");
+		}
+				
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 			
-			String password = scan.nextLine();
+		return username;
+	}
+		
+	//retrieve database password and compare to entered password.  If they do not match, return to home page.
+	public static void getPassword(Connection conn, String username) {
 			
-			resultSet = stmt.executeQuery("select password from logins where username = '"+username+"'");
+		System.out.println("Please enter your password.");
+			
+		String password = utility.scan.nextLine();
+		
+		String dbPassword = null;
+			
+		try {
+			Statement stmt = conn.createStatement();
+				
+			ResultSet resultSet = stmt.executeQuery("select password from logins where username = '"+username+"'");
 			
 			resultSet.next();
-			
-			String dbPassword = resultSet.getString("password");
-			
-			if (password.equals(dbPassword)) {
-				dashboard.dashboardMenu();
+				
+			dbPassword = resultSet.getString("password");
+				
+			if (!password.equals(dbPassword)) {
+				System.out.println("The password you entered does not match our records.  Please try again.\n");
+				login.signIn();
 			}
-			
 		}
 			
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+		
+	//Update SQL database with new user-supplied password
+	public static void updatePassword(Connection conn, String username) {
+			
+		System.out.println("Please enter a new password.");
+			
+		String newPassword = utility.scan.nextLine().toLowerCase();
+			
+		try {
+			Statement stmt = conn.createStatement();
+				
+			stmt.executeUpdate("Update logins set password = '"+newPassword+"' where username = '"+username+"'");
+				
+			System.out.println("Your password has been updated.  Please login again with your new credentials.\n");
+				
+			login.signIn();
+				
+		} catch (SQLException e) {
+				e.printStackTrace();
+		}
+	}
 	
-	
-	public static void createAccount() {
+	//update SQL database with new username and password
+	public static void createAccount(Connection conn) {
 		System.out.println("Please enter a username.  Type \"Home\" to return to the main screen.");
-		
-		String username = scan.nextLine().toLowerCase();
-		
+			
+		String username = utility.scan.nextLine().toLowerCase();
+			
 		if (username.equals("home")) {
-			startup();
+			login.startup();
 		}
-		
-		//Connect to database.  Make sure username entered is unique, prompt for password, redirect as appropriate.
+			
 		try {
-			
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://ziggy.db.elephantsql.com/wxjpiraq","wxjpiraq","D6DpgE-lOUMD-QRrIBLiSUBhmxs5FW-_");
-			
+				
 			Statement stmt = conn.createStatement();
-			
+				
 			ResultSet resultSet = stmt.executeQuery("select username from logins where username = '"+username+"'");
-			
-			if (resultSet.next() == false) {
-				System.out.println("Please enter a password");
 				
-				String password = scan.nextLine();
-				
-				stmt.executeUpdate("Insert into logins (username,password) values ('"+username+"','"+password+"')");
-				
-				System.out.println("Your account has been created.  Please login using your new credentials.\n");
-				
-				startup();
+			if (resultSet.next()) {
+				System.out.println("That username already exists.\n");
+				createAccount(conn);
 			}
-			
-			else {
-				System.out.println("That username already exists in the database.  Please choose a different username.");
-				createAccount();
-			}
-			
-		} catch (SQLException e) {
+				
+			System.out.println("Please create a password for your account.");
+				
+			String password = utility.scan.nextLine();
+				
+			stmt.executeUpdate("Insert into logins (username, password) values ('"+username+"','"+password+"')");
+				
+			System.out.println("Your account has been created.  Please login using your new credentials.\n");
+			login.signIn();
+		}	
+		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-	}
-
-	public static void resetPassword() {
-		System.out.println("Please enter your username.  Type \"Home\" to return to the main screen.");
-		
-		String username = scan.nextLine().toLowerCase();
-		
-		if (username.equals("home")) {
-			startup();
-		}
-		
-		//Connect to database.  Compare entered values to database values, prompt for new password.  Redirect as appropriate.
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://ziggy.db.elephantsql.com/wxjpiraq","wxjpiraq","D6DpgE-lOUMD-QRrIBLiSUBhmxs5FW-_");
-			
-			Statement stmt = conn.createStatement();
-			
-			ResultSet resultSet = stmt.executeQuery("select username from logins where username = '"+username+"'");
-			
-			if (resultSet.next() == false) {
-				System.out.println("That username does not exist in our system.  Please check your spelling and try again or create a new account.\n");
-				resetPassword();
-			}		
-			
-			System.out.println("Please enter your current password.");
-			
-			String enteredPassword = scan.nextLine();
-			
-			resultSet = stmt.executeQuery("Select password from logins where username = '"+username+"'");
-			
-			resultSet.next();
-			
-			String currentPassword = resultSet.getString("Password");
-			
-			if (enteredPassword.equals(currentPassword)) {
-				System.out.println("Please enter a new password.");
-				
-				String newPassword = scan.nextLine();
-				
-				stmt.executeUpdate("Update logins set password = '"+newPassword+"' where username = '"+username+"'");
-				
-				System.out.println("Your password has been updated.  Please login again using your new credentials.\n");
-				
-				startup();
-			}
-			
-			else {
-				System.out.println("The password you entered does not match our records.  Please try again.\n");
-				resetPassword();
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
+	}	
 }
